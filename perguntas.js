8811eb5e7f455f7780e825d1a7a2ee122c8b58df
Pantufinhas <168852053+Pantufinhas@users.noreply.github.com>
2024-11-29 13:39:33 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'economia',
+    'O que é o fundo LCA?',
+    'A LCA é um investimento de renda fixa criado para dar lastro ao crédito agropecuário no país. Os títulos são emitidos pelo BB com o objetivo de captar recursos financeiros destinados a empréstimos, neste caso, para o setor agropecuário.'
+)
+
+criaCartao(
+    'Economia',
+    'O que é o fundo LCI?',
+    'A LCI é um investimento de renda fixa criado para apoiar o financiamento imobiliário no país. É emitida com o objetivo de captar recursos financeiros destinados a empréstimos para o setor imobiliário.'
+)
+
+criaCartao(
+    'Economia',
+    'É possível viver de fundos imobiliários?',
+    'Por exemplo, se você deseja um rendimento mensal de R$ 10 mil, R$ 15 mil ou R$ 25 mil e o dividend yield mensal do FII é de 0,5%, você precisaria ter investido R$ 2 milhões, R$ 3 milhões e R$ 5 milhões, respectivamente. Mas isso considerando que a rentabilidade de um bom FII é de 0,5% a 1% ao mês, em média'
+)
+
+criaCartao(
+    'Economia',
+    'Lista de melhores bancos para começar os seus investimentos passivos',
+    'NuBack / pagbank / c6 bank / Bradesco'
+)
